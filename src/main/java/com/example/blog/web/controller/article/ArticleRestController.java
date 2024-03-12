38e459470947045e17f76c5c9b7e49c2fce8a5ef
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-03-12 16:01:25 +0900

    3層アーキテクチャを導入する - Service/Entity クラスの作成

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,22 +1,24 @@
 package com.example.blog.web.controller.article;
 
+import com.example.blog.service.article.ArticleService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-
 @RestController
 public class ArticleRestController {
 
+    private final ArticleService articleService = new ArticleService();
+
     @GetMapping("/articles/{id}")
     public ArticleDTO showArticle(@PathVariable("id") long id) {
+        var entity = articleService.findById(id);
         return new ArticleDTO(
-                id,
-                "This is title: id = " + id,
-                "This is content",
-                LocalDateTime.now(),
-                LocalDateTime.now()
+                entity.id(),
+                entity.title(),
+                entity.content(),
+                entity.createdAt(),
+                entity.updatedAt()
         );
     }
 }
