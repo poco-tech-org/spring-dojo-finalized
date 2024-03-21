dce84b95b7311a1e8e8cfbf5c1d4e836025555bc
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-03-21 10:26:12 +0900

    ログインのエンドポイントは CSRF 対策の対象から除外する

--- src/main/java/com/example/blog/config/SecurityConfig.java
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/login"))
                 .addFilterAt(new JsonUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/articles/**").permitAll()
