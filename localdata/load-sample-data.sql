149c2ca15e0154dd0a2942f594f6e1fc51097480
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-03-21 17:38:06 +0900

    ユーザー管理用のテーブルを作る

--- localdata/load-sample-data.sql
+++ localdata/load-sample-data.sql
@@ -1,3 +1,7 @@
 INSERT INTO articles (title, body) VALUES ('タイトルです1', '1本文です。');
 INSERT INTO articles (title, body) VALUES ('タイトルです2', '2本文です。');
 INSERT INTO articles (title, body) VALUES ('タイトルです3', '3本文です。');
+
+INSERT INTO users (username, password, enabled) VALUES ('user1', 'password1', true);
+INSERT INTO users (username, password, enabled) VALUES ('user2', 'password2', true);
+INSERT INTO users (username, password, enabled) VALUES ('user3', 'password1', true);
